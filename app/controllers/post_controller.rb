6702388e1e78f5d0d407a5b073d8e9afa9b76d91
albert <albert@localhost>
2009-04-15 23:49:50 +0000

    Merged revisions 2045-2047 via svnmerge from svn+ssh://albert@donmai.us/var/cache/svn/danbooru/trunk
    ........
      r2046 | albert | 2009-04-15 19:26:41 -0400 (Wed, 15 Apr 2009) | 4 lines
      
      * Only privileged users can vote
      * Migration to reset scores based on priv fav count
      * Updated tests
    ........
      r2047 | albert | 2009-04-15 19:49:12 -0400 (Wed, 15 Apr 2009) | 2 lines
      
      * View/test updates for new voting rules
    ........

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -318,14 +318,17 @@ class PostController < ApplicationController
   def vote
     p = Post.find(params[:id])
     score = params[:score].to_i
-    
+    respond_to_params = {:action => "show", :id => params[:id], :tag_title => p.tag_title}
+  
     begin
-      p.vote!(@current_user, score, request.remote_ip)
-      respond_to_success("Vote saved", {:action => "show", :id => params[:id], :tag_title => p.tag_title}, :api => {:score => p.score, :post_id => p.id})
+      p.vote!(@current_user, score)
+      respond_to_success("Vote saved", respond_to_params, :api => {:score => p.score, :post_id => p.id})
     rescue PostVoteMethods::InvalidScoreError
-      respond_to_error("Invalid score", {:action => "show", :id => params[:id], :tag_title => p.tag_title}, :status => 424)
+      respond_to_error("Invalid score", respond_to_params, :status => 424)
     rescue PostVoteMethods::AlreadyVotedError
-      respond_to_error("Already voted", {:action => "show", :id => params[:id], :tag_title => p.tag_title}, :status => 423)
+      respond_to_error("Already voted", respond_to_params, :status => 423)
+    rescue PostVoteMethods::PrivilegeError
+      respond_to_error("Only privileged members can vote", respond_to_params, :status => 403)
     end
   end
 
