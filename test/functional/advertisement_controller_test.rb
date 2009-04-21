dc6ad6074fabb22eefd51da5bc771c74b7754729
albert <albert@localhost>
2009-04-21 22:52:11 +0000

    Merged revisions 2067-2076 via svnmerge from svn+ssh://albert@donmai.us/var/cache/svn/danbooru/trunk
    ........
      r2068 | albert | 2009-04-20 18:25:37 -0400 (Mon, 20 Apr 2009) | 2 lines
      
      * Added functional tests for advertisements
    ........
      r2069 | albert | 2009-04-20 18:29:37 -0400 (Mon, 20 Apr 2009) | 1 line
      
      finished ad tests
    ........
      r2072 | albert | 2009-04-20 21:25:32 -0400 (Mon, 20 Apr 2009) | 2 lines
      
      * Temporarily disabled blacklists
    ........
      r2073 | albert | 2009-04-20 22:48:42 -0400 (Mon, 20 Apr 2009) | 1 line
      
      removed voteup
    ........
      r2075 | albert | 2009-04-21 18:50:58 -0400 (Tue, 21 Apr 2009) | 1 line
      
      cookie fix
    ........

---
+++ test/functional/advertisement_controller_test.rb
@@ -0,0 +1,46 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class AdvertisementControllerTest < ActionController::TestCase
+  fixtures :users, :advertisements
+  
+  def test_index
+    get :index
+    assert_response :success
+  end
+  
+  def test_create
+    assert_equal(2, Advertisement.count)
+    
+    get :new
+    assert_response 302
+    
+    get :new, {}, {:user_id => 1}
+    assert_response :success
+    
+    post :create, {:ad => {:image_url => "test", :referral_url => "test", :ad_type => "vertical", :status => "active", :width => "500", :height => "500", :is_work_safe => "1"}}, {:user_id => 1}
+    assert_equal(3, Advertisement.count)
+    ad = Advertisement.find(:last)
+    assert_equal("test", ad.image_url)
+  end
+  
+  def test_update
+    get :edit, {:id => 1}
+    assert_response 302
+    
+    get :edit, {:id => 1}, {:user_id => 1}
+    assert_response :success
+    
+    post :update, {:id => 1, :ad => {:image_url => "test", :referral_url => "test", :ad_type => "vertical", :status => "active", :width => "500", :height => "500", :is_work_safe => "1"}}, {:user_id => 1}
+    ad = Advertisement.find(1)
+    assert_equal("test", ad.image_url)
+  end
+  
+  def test_redirect
+    assert_equal(0, AdvertisementHit.count)
+    get :redirect_ad, {:id => 1}
+    assert_redirected_to "referral url"
+    assert_equal(1, AdvertisementHit.count)
+    assert_equal(1, AdvertisementHit.first.advertisement_id)
+    assert_equal("0.0.0.0", AdvertisementHit.first.ip_addr)
+  end
+end
