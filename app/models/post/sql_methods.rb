7887f6f352e2b8c7b4458edc9316fa8715f635a7
albert <albert@localhost>
2009-10-27 23:28:19 +0000

    Merged revisions 2279-2280 via svnmerge from svn+ssh://albert@donmai.us/var/cache/svn/danbooru/trunk
    ........
      r2280 | albert | 2009-10-27 19:27:53 -0400 (Tue, 27 Oct 2009) | 3 lines
      
      * Fixed references to sanitize_sql (2.3.4)
      * Fixed actionmailer tests (2.3.4)
    ........

--- app/models/post/sql_methods.rb
+++ app/models/post/sql_methods.rb
@@ -252,7 +252,7 @@ module PostSqlMethods
       
       params = join_params + cond_params
 
-      return Post.sanitize_sql([sql, *params])
+      return Post.sanitize_sql_array([sql, *params])
     end
   end
   
